95445dbece875979bc58b7aa7017c03e6bd75544
pengliangliang <pengliangliang841010@outlook.com>
2023-10-29 22:12:56 +0800

    优化

--- components/CountTime.tsx
+++ components/CountTime.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useRef, useState } from "react";
 
 export default function Countime(props) {
-    let { shelfTime } = props; // 组件传过来的时间戳
+    const { shelfTime } = props; // 组件传过来的时间戳
     const timer = useRef<any>(0);
     const [remaining, setRemaining] = useState<any>({
         day: 0,
@@ -23,16 +23,16 @@ useEffect(() => {
 
 function countFun (end){
     // 注意  Date.parse(new Date()) 获取是13位的时间戳;
-    let now_time = Date.parse(new Date().toString());
-    var remaining = end - now_time;
+    const now_time = Date.parse(new Date().toString());
+    let remaining = end - now_time;
     timer.current = setInterval(() => {
         //防止出现负数
         if (remaining > 1000) {
             remaining -= 1000;
-            let day = Math.floor(remaining / 1000 / 3600 / 24);
-            let hour = Math.floor((remaining / 1000 / 3600) % 24);
-            let minute = Math.floor((remaining / 1000 / 60) % 60);
-            let second = Math.floor((remaining / 1000) % 60);
+            const day = Math.floor(remaining / 1000 / 3600 / 24);
+            const hour = Math.floor((remaining / 1000 / 3600) % 24);
+            const minute = Math.floor((remaining / 1000 / 60) % 60);
+            const second = Math.floor((remaining / 1000) % 60);
             setRemaining({
                 day: day,
                 hour: hour < 10 ? '0' + hour : hour,
--- components/Discount.tsx
+++ components/Discount.tsx
@@ -3,12 +3,16 @@ import { NextPage } from "next"
 import React, { useEffect, useState } from "react"
 import Styles from '../styles/Discount.module.scss'
 import Countime from "./CountTime"
+import { useRouter } from 'next/router'
 
 const Discount: NextPage = () => {
 
     const [show, setShow] = useState<boolean>(true)
 
-    const close = () => {
+    const router=useRouter()
+
+    const close = (e) => {
+        e.stopPropagation()
         setShow(false)
         document.documentElement.style.setProperty('--discountHeight', '0px')
     }
@@ -20,7 +24,9 @@ const Discount: NextPage = () => {
         }
     }, [])
 
-    return <>{show?<div className={Styles.wrap}>
+    return <>{show?<div className={Styles.wrap} onClick={()=>{
+        router.push('/subcribe')
+    }}>
         <div className={Styles.width1280}>
             <div className={Styles.inner}>
                 <div className={Styles.left}>
@@ -31,8 +37,8 @@ const Discount: NextPage = () => {
                 </div>
                 <div className={Styles.right}>
                     <Countime shelfTime={dayjs().add(1, 'day').valueOf()} />
-                    <span onClick={() => { close() }}>
-                        <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4244" width="24" height="24"><path d="M512 128C300.8 128 128 300.8 128 512s172.8 384 384 384 384-172.8 384-384S723.2 128 512 128zM512 832c-179.2 0-320-140.8-320-320s140.8-320 320-320 320 140.8 320 320S691.2 832 512 832z" p-id="4245" fill="#ffffff"></path><path d="M672 352c-12.8-12.8-32-12.8-44.8 0L512 467.2 396.8 352C384 339.2 364.8 339.2 352 352S339.2 384 352 396.8L467.2 512 352 627.2c-12.8 12.8-12.8 32 0 44.8s32 12.8 44.8 0L512 556.8l115.2 115.2c12.8 12.8 32 12.8 44.8 0s12.8-32 0-44.8L556.8 512l115.2-115.2C684.8 384 684.8 364.8 672 352z" fill="#ffffff"></path></svg>
+                    <span onClick={close}>
+                        <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M512 128C300.8 128 128 300.8 128 512s172.8 384 384 384 384-172.8 384-384S723.2 128 512 128zM512 832c-179.2 0-320-140.8-320-320s140.8-320 320-320 320 140.8 320 320S691.2 832 512 832z" fill="#ffffff"></path><path d="M672 352c-12.8-12.8-32-12.8-44.8 0L512 467.2 396.8 352C384 339.2 364.8 339.2 352 352S339.2 384 352 396.8L467.2 512 352 627.2c-12.8 12.8-12.8 32 0 44.8s32 12.8 44.8 0L512 556.8l115.2 115.2c12.8 12.8 32 12.8 44.8 0s12.8-32 0-44.8L556.8 512l115.2-115.2C684.8 384 684.8 364.8 672 352z" fill="#ffffff"></path></svg>
                     </span>
                 </div>
             </div>
